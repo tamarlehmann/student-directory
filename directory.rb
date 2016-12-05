592af0f3ccc6b1eac1cb1019dc612c55c7c88a03
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-12-05 16:34:19 +0000

    Ex.07 User Input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-#let's put all students into an array
-students = [
-  {name:"Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name:"Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student has to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get naother name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of the Villains Academy"
@@ -27,10 +31,9 @@ end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
+
 # nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
-
-# note, we are passing the students variable to the methods as an argument (called names)
-# because the methods don't have access to local variables defined outside them.
