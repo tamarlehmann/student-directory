dd380b121471ecfa861ae379aead04ae25d1b498
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-12-06 21:21:12 +0000

    Exercise 12

---
+++ Exercises/Ex8_12.rb
@@ -0,0 +1,52 @@
+# What happens if the user doesn't enter any students? It will try to print an empty list.
+# How can you use an if statement to only print the list if there is at least one student in there?
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student has to the array
+    students << {name: name, cohort: :november}
+    if students.length == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    #get naother name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of the Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  if students.length == 1
+    puts "Overall, we have #{students.count} great student"
+  elsif students.length > 1
+    puts "Overall, we have #{students.count} great students"
+  else
+    puts "There are no students enrolled at the Villains Academy"
+  end
+end
+
+# nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
