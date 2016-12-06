565d8b393e2f314a7a191f7606bf554f28305bdb
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-12-06 19:56:35 +0000

    Exercise 9

---
+++ Exercises/Ex8_9.rb
@@ -0,0 +1,47 @@
+#Right now if we have only one student, the user will see a message "Now we have 1
+# students", whereas it should be "Now we have 1 student". How can you fix it so that
+# it used singular form when appropriate and plural form otherwise?
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student has to the array
+    students << {name: name, cohort: :november}
+    if students.length == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+    #get naother name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of the Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+# nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
