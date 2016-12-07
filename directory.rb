e819227155aa55e9b5c58586dc259c11dbd389b6
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-12-07 13:08:34 +0000

    Refactoring the code and changing away from local variables

--- directory.rb
+++ directory.rb
@@ -1,49 +1,55 @@
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
+@students = [] # an empty array accessible to all methods
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     #add the student has to the array
-    students << {name: name, cohort: :november}
-    if students.length == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: :november}
+    if @students.length == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
-    #get naother name from the user
+    #get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
+end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+    case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
 end
 
 def print_header
@@ -51,19 +57,20 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)" # note: no '@' on string interpolation as referencing
+    # an iteration of the students array, called student.
   end
 end
 
-def print_footer(students)
-  if students.length == 1
-    puts "Overall, we have #{students.count} great student"
-  elsif students.length > 1
-    puts "Overall, we have #{students.count} great students"
+def print_footer
+  if @students.length == 1
+    puts "Overall, we have #{@students.count} great student"
+  elsif @students.length > 1
+    puts "Overall, we have #{@students.count} great students"
   end
 end
 
 # nothing happens until we call the methods
-students = interactive_menu
+interactive_menu
