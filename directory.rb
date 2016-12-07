152070a72f3c20c4ada58e572347162240978cbc
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-12-07 12:09:45 +0000

    Added the Interactive Menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -9,7 +34,11 @@ def input_students
   while !name.empty? do
     #add the student has to the array
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    if students.length == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     #get naother name from the user
     name = gets.chomp
   end
@@ -29,11 +58,12 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.length == 1
+    puts "Overall, we have #{students.count} great student"
+  elsif students.length > 1
+    puts "Overall, we have #{students.count} great students"
+  end
 end
 
 # nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+students = interactive_menu
