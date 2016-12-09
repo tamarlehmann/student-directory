91b74f81a80035dd8bb4f1239407661ce8a722cc
Tamar Lehmann <tlehmann@hotmail.co.uk>
2016-12-09 14:15:29 +0000

    Exercise 14.3: Refactoring the code

---
+++ Exercises/Ex14_3.rb
@@ -0,0 +1,128 @@
+# Continue refactoring the code. Which method is a bit too long? What method names
+# are not clear enough? Anything else you'd change to make your code look more elegant? Why?
+
+@students = [] # an empty array accessible to all methods
+
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = STDIN.gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array using student method
+    add_students(name)
+    puts @students.length == 1 ? ("Now we have #{@students.count} student") : ("Now we have #{@students.count} students")
+    #get another name from the user
+    name = STDIN.gets.chomp
+  end
+end
+
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  if filename.nil?
+    load_students
+    puts "Loaded #{@students.count} from students.csv"
+  elsif File.exists?(filename) # if file passed as ARGV.first exists
+    load_students(filename) # go to method load students with filename as argument
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if the file doesn't exist
+    puts "Sorry, #{filename} doesnt exist"
+    exit # quit the program
+  end
+end
+
+
+def load_students(filename = "students.csv")
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    add_students(name, cohort)
+  file.close
+end
+end
+
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "a")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+
+def interactive_menu
+  loop do
+    print_menu
+    menu_selection(STDIN.gets.chomp)
+  end
+end
+
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+
+def menu_selection(selection)
+    case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+end
+
+
+def print_header
+  puts "The students of the Villains Academy"
+  puts "-------------"
+end
+
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+
+def print_footer
+  puts @students.length == 1 ? ("Overall, we have #{@students.count} great student") : ("Overall, we have #{@students.count} great students")
+end
+
+
+def add_students(name, cohort = :november) # pass arguments name and cohort, if cohort not specified
+  # which it isn't in the method input students, then use november as default.
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
+# nothing happens until we call the methods
+try_load_students
+interactive_menu
